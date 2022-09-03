357882ab82857e6a96deb8e1ed4eda72c34c5b1a
Alexander <castro.t.alex@gmail.com>
2022-09-03 12:43:21 -0500

    actualizacion de meta tags para redes sociales

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -1,13 +1,19 @@
 import Head from "next/head"
 import { Navbar } from '../ui';
 
+
  
 interface LayoutProps {
   children: React.ReactNode
   title?: string
 }
 
+const origin = (typeof window === 'undefined') ? '' : window.location.origin;
+
 export const Layout = ({children, title}: LayoutProps) => {
+
+
+ 
   return (
     <>
     <Head>
@@ -15,6 +21,10 @@ export const Layout = ({children, title}: LayoutProps) => {
       <meta name="author" content="Alex Castro"/>
       <meta name="description" content="Pokemon name description"/>
       <meta name="keywords" content="name, pokedex, pokemon"/>
+
+      <meta property="og:title" content={`Informacion sobre el pokémon ${title}`} />
+      <meta property="og:description" content={`Esta es la página sobre ${title}`} />
+      <meta property="og:image" content={`${origin}/image/banner.png`} />
     
     </Head>
     <Navbar/>
